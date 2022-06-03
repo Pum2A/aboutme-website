4f277425043a174bf59a68ddc8df32c2f650216e
Dominik_Gadzina <epros98@gmail.com>
2022-06-03 16:48:15 +0200

    search script added & works

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 const hamburger = document.querySelector('.top span')
 const nav = document.querySelector('nav');
 const styleNav = getComputedStyle(nav);
-const search = document.querySelector('.search')
+const search = document.querySelector('#search')
+const searchInput = document.querySelector('#inputSearch')
+const styleSearchInput = getComputedStyle(inputSearch)
 
 hamburger.addEventListener('click', () => {
     if(styleNav.display == 'none') {
@@ -14,19 +16,19 @@ hamburger.addEventListener('click', () => {
     } else {
         nav.style.display = 'none'
         hamburger.innerText = 'menu';
+        
     }
 
 });
 
 search.addEventListener('click', () => {
-    if(styleNav.display == 'none') {
+    if(styleSearchInput.display == 'none') {
 
-        nav.style.display = 'flex';
+        searchInput.style.display = 'flex';
         search.innerText = 'close';
-        nav.style.left = '0%'
 
     } else {
-        nav.style.display = 'none'
+        searchInput.style.display = 'none'
         search.innerText = 'search';
     }
-})
+});
